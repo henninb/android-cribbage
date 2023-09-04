01f5decd8aee3d9112b69e92d0554ffbb534779f
Brian Henning <henninb@msn.com>
2023-09-04 16:46:36 -0500

    initial

---
+++ app/src/main/java/com/bhenning/simple/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.bhenning.simple
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.bhenning.simple.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var bottomNavigationView: BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+        bottomNavigationView.setupWithNavController(navController)
+    }
+}
---
+++ app/src/main/java/com/bhenning/simple/ThirdFragment.kt
@@ -0,0 +1,44 @@
+package com.bhenning.simple
+
+import android.annotation.SuppressLint
+import android.os.Build
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.annotation.RequiresApi
+import com.bhenning.simple.databinding.ThirdFragmentBinding
+
+class ThirdFragment : Fragment() {
+    private var _binding: ThirdFragmentBinding? = null
+    private val binding get() = _binding!!
+    lateinit var mywebView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = ThirdFragmentBinding.inflate(inflater, container, false)
+        //mywebView = findViewById(R.id.thirdFragment)
+        mywebView = binding.webView
+        mywebView.webViewClient = WebViewClient()
+        mywebView.apply {
+            loadUrl("https://www.google.com/")
+            settings.javaScriptEnabled = true
+            settings.safeBrowsingEnabled = true
+        }
+        return binding.root
+    }
+
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
