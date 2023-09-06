d38b1c6f77ac476ba28743ab7ad5c2bce412ac2c
Brian Henning <henninb@msn.com>
2023-09-06 07:28:40 -0500

    wip

---
+++ app/src/main/java/com/bhenning/simple/MyApplication.kt
@@ -0,0 +1,11 @@
+package com.bhenning.simple
+
+import android.app.Application
+import android.os.Bundle
+
+class MyApplication : Application() {
+    // Application-level initialization code goes here.
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//
+//    }
+}
