643e6f12c74bc6575e476b6761fc4d7b4001143e
Brian Henning <henninb@msn.com>
2023-12-02 16:17:29 -0600

    wip

--- app/src/main/java/com/bhenning/example/ScheduleService.kt
+++ app/src/main/java/com/bhenning/example/ScheduleService.kt
@@ -3,6 +3,7 @@ package com.bhenning.example
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.POST
 
 //  curl -X POST https://cflare.bhenning.com/api/login -H "Content-Type: application/json" -d '{"email":"henninb@gmail.com", "password":"monday1"}'
@@ -12,6 +13,12 @@ interface ScheduleService {
     @POST("api/login")
     fun login(@Body loginRequest: LoginRequest): Call<String>
 
+//    @Headers({
+//        "Accept: application/vnd.yourapi.v1.full+json",
+//        "User-Agent: Your-App-Name"
+//    })
+
+    @Headers("Cache-Control: max-age=640000")
     @GET("feed/json/nhl-2023/minnesota-wild")
     fun schedule() : Call<Array<ApiResponse>>
 }
